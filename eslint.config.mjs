edfb6a83ae89340186e886f565a963e378e0cb41
Pasha Naumov <pavliknaumov@gmail.com>
2026-06-24 14:38:20 +0100

    feat(arch-btw-aw5): fix eslint config for ESLint 10 + typescript-eslint flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+  },
+  {
+    ignores: [".next/**", "node_modules/**", "drizzle/**"],
+  }
+);
